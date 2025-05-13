5730579d1e5a552dd2516ff3317ef80310431369
khushichhari <chharikhushi8@gmail.com>
2025-05-13 23:27:32 +0530

    Deleted unnecessary files and updated App.jsx

--- dairy/src/App.jsx
+++ dairy/src/App.jsx
@@ -1,4 +1,3 @@
-// Original
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import LoginRegisterToggle from "./components/LoginRegisterToggle"; // Import the LoginRegisterToggle component
 import Home from "./components/Home.jsx"; // Import the Home component
