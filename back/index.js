64a5085bba367353a012b006eca6282068aac6a8
khushichhari <130910904+khushichhari@users.noreply.github.com>
2025-05-15 23:53:31 +0530

    Update index.js

--- back/index.js
+++ back/index.js
@@ -14,10 +14,7 @@ app.use(express.json());
 
 // MongoDB Connection
 mongoose
-  .connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGODB_URI)
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.error(err));
 
