0e2ee753981a5802c188a046ee2aac684b444ce0
khushichhari <130910904+khushichhari@users.noreply.github.com>
2025-05-16 14:04:57 +0530

    Update TodosIn.jsx

--- dairy/src/components/TodosIn.jsx
+++ dairy/src/components/TodosIn.jsx
@@ -5,7 +5,7 @@ import axios from 'axios';
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL; // Add this line
 
 export default function TodosIn({ onAddTask }) {
-  const [task, setTask] = useState('');
+  const [task, setTask] = useState( ' ' );
 
   const handleAdd = () => {
     if (task.trim() === '') {
