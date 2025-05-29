77e448c879274b425cd8eede966fe11ef66c4d97
khushichhari <130910904+khushichhari@users.noreply.github.com>
2025-05-29 15:14:41 +0530

    Update vite.config.js

--- dairy/vite.config.js
+++ dairy/vite.config.js
@@ -1,8 +1,19 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // This is Vite's default, added explicitly for clarity
+  },
 })
